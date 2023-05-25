193cdf2fd3a62776be38cf3562c1d025c697e32c
francinjr <francinaldojunior@softmedical.com.br>
2023-05-25 08:57:13 -0300

    Projeto Criado

---
+++ src/test/java/com/francin/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francin.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
