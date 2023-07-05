946e1222e2614d001ce464f5a83fde440909ce16
francinjr <francinaldojunior@softmedical.com.br>
2023-07-05 19:42:40 -0300

    GET game by id

--- src/main/java/com/francin/dslist/services/GameService.java
+++ src/main/java/com/francin/dslist/services/GameService.java
@@ -2,10 +2,11 @@ package com.francin.dslist.services;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.francin.dslist.dto.GameDTO;
 import com.francin.dslist.dto.GameMinDTO;
 import com.francin.dslist.entities.Game;
 import com.francin.dslist.repositories.GameRepository;
@@ -16,6 +17,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
