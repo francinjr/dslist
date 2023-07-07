da672c4353540b1e39d842da6707b27afe17f15a
francinjr <francinaldojunior@softmedical.com.br>
2023-07-07 12:24:26 -0300

    Homologação funcionando

--- src/main/java/com/francin/dslist/projections/GameMinProjection.java
+++ src/main/java/com/francin/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.francin.dslist.projections;
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
